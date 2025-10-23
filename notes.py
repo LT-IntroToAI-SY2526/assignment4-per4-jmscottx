be8943217893d28caafa222d11b5f0cae7ce8ef1
James Scott <jmscott9@cps.edu>
2025-10-23 11:20:06 -0500

    102325

--- notes.py
+++ notes.py
@@ -2,3 +2,36 @@
 
 # (define-struct dog [fur_color name age favorite_food])
 
+class Dog: 
+    """
+    A simple Dog class to learn OOP concepts
+
+    Attributes
+    breed - the breed of the dog
+    fur_color - the fur color of the dog
+    name - the name of the dog
+    age - the age of the dog
+    """
+
+    def __init__(self, breed, fur_color, name, age):
+        """Initialize a new Dog with breed, fur_color, name and age"""
+        self.breed = breed
+        self.fur_color = fur_color
+        self.name = name
+        self.age = age
+
+    def __str__(self):
+        """String representation of a dog"""
+        return f"{self.name} is a {self.age} year old {self.fur_color} {self.breed}"
+
+    def bark(self): 
+        return f"{self.name} says: Woof, Woof!!"
+
+if __name__ == "__main__":
+    berg_dog = Dog("labrador", "black", "logan", 9)
+    aidan_dog = Dog("lab pitt mix", "grey", "cubbie", 9)
+
+    print(berg_dog)
+    print(aidan_dog)
+
+    print(aidan_dog.bark())
